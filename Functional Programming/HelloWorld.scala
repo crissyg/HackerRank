9a7ed0f1468c30604720830fbb8013a2d48f8084
crissyg <crissyg@users.noreply.github.com>
2017-08-07 19:00:17 -0400

    Hello World in Scala
    Hello World in Scala

---
+++ Functional Programming/HelloWorld.scala
@@ -0,0 +1,8 @@
+/** Functional Programming  - Hello World
+* Submitted by: Christina Gordon
+* https://www.hackerrank.com/challenges/fp-hello-world/problem
+*/
+
+/** Hello World in Scala */
+
+def f() = println("Hello World")
