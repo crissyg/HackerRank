ba71fb0e874586e5fe5ee8d0406e27ecb35d6e8f
crissyg <crissyg@users.noreply.github.com>
2017-08-07 18:27:16 -0400

    Day 3
    Day 3: Conditional Statements

---
+++ 30DaysofCode/Operators.java
@@ -0,0 +1,31 @@
+// Day 2: Operators
+// Submitted by: Christina Gordon
+// https://www.hackerrank.com/challenges/30-operators/tutorial
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+      
+        // Write your calculation code here.
+        double tip = (mealCost * (tipPercent*.010));
+        double tax = (mealCost * (taxPercent*.010));
+        double total = (mealCost + tip + tax);
+        // cast the result of the rounding operation to an int and save it as totalCost 
+        int totalCost = (int) Math.round(total);
+      
+        // Print your result
+        System.out.println("The total meal cost is "+totalCost+" dollars.");
+    }
+}
--- 30DaysofCode/dataTypes.java
+++ 30DaysofCode/dataTypes.java
@@ -1,21 +1,30 @@
 // Day 1: Data Types
 // Submitted by: Christina Gordon
 // https://www.hackerrank.com/challenges/30-data-types/problem
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+	public static void main(String[] args) {
 /* Declare second integer, double, and String variables. */
- int ii;
- double dd;
- String ss;   
-/* Read and save an integer, double, and String to your variables.*/
-// Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
-  ii = scan.nextInt();
-  dd = scan.nextDouble();
-  scan.nextLine(); 
-  ss = scan.nextLine();
-/* Print the sum of both integer variables on a new line. */
-System.out.println(i+ii); 
-/* Print the sum of the double variables on a new line. */
-System.out.println(d+dd); 
-/* Concatenate and print the String variables on a new line; 
-	the 's' variable above should be printed first. */
-System.out.println(s+ss); 
+	 int ii;
+	 double dd;
+	 String ss;   
+	/* Read and save an integer, double, and String to your variables.*/
+	// Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+	  ii = scan.nextInt();
+	  dd = scan.nextDouble();
+	  scan.nextLine(); 
+	  ss = scan.nextLine();
+	/* Print the sum of both integer variables on a new line. */
+	System.out.println(i+ii); 
+	/* Print the sum of the double variables on a new line. */
+	System.out.println(d+dd); 
+	/* Concatenate and print the String variables on a new line; 
+		the 's' variable above should be printed first. */
+	System.out.println(s+ss); 
+		}
+}
